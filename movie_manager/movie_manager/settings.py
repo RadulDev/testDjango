e9cf8c8ed9b3a677e6210def4eb1fe1dbd045008
RadulDev <rrdevattract@gmail.com>
2024-02-13 12:40:13 +0530

    DB connect

--- movie_manager/movie_manager/settings.py
+++ movie_manager/movie_manager/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'movie_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movies_db',
+        'HOST' :'127.0.0.1',
+        'PORT' : '5432',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres'
     }
 }
 
